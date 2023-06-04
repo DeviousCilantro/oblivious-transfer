ddc81d17f6eba9cdd81e15dbd9a3853b4477316d
DeviousCilantro <archisman@mailbox.org>
2023-06-05 05:06:56 +0530

    Implement TCP socket communication

--- alice/src/main.rs
+++ alice/src/main.rs
@@ -1,7 +1,8 @@
 use num_primes::Generator;
 use std::io;
-use std::io::Write;
 use rug::{Integer, rand};
+use std::net::TcpStream;
+use std::io::{prelude::*,BufReader,Write};
 
 fn generate_keypair() -> ((Integer, Integer), Integer) {
     println!("Generating keypair...");
@@ -21,20 +22,19 @@ fn generate_keypair() -> ((Integer, Integer), Integer) {
     ((n, e), d)
 }
 
-fn combine_values(v: &Integer, randoms: &Vec<Integer>, d: &Integer, n: &Integer, messages: &[Integer]) -> Vec<Integer> {
-    let mut ciphertexts: Vec<Integer> = Vec::new();
+fn combine_values(v: &Integer, randoms: &Vec<Integer>, d: &Integer, n: &Integer, messages: &[Integer]) -> Vec<String> {
+    let mut ciphertexts: Vec<String> = Vec::new();
     for i in 0..randoms.len() {
-        ciphertexts.push(messages[i].clone() + Integer::secure_pow_mod(v.clone() - randoms[i].clone(), d, n));
+        let mi = messages[i].clone() + Integer::secure_pow_mod(v.clone() - randoms[i].clone(), d, n);
+        ciphertexts.push(base64::encode(mi.to_string()));
     }
     ciphertexts
 }
 
 fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:6969").expect("Failed to connect");
     let ((n, e), d) = generate_keypair();
-    println!("\nPublic key: (n, e)");
-    println!("n: {}", base64::encode(n.to_string()));
-    println!("e: {}", base64::encode(e.to_string()));
-    println!("\nSecret key: {}", base64::encode(d.to_string()));
+    let mut alice_sends_bob: Vec<String> = Vec::new();
     let mut input = String::new();
     let mut messages: Vec<Integer> = Vec::new();
     let mut randoms: Vec<Integer> = Vec::new();
@@ -44,9 +44,10 @@ fn main() {
         .read_line(&mut input)
         .unwrap();
     let number = input.trim().parse().unwrap();
+    println!();
     for i in 0..number {
         let mut input = String::new();
-        print!("\nEnter m{i}: ");
+        print!("Enter message {i}: ");
         io::stdout().flush().unwrap();
         io::stdin()
             .read_line(&mut input)
@@ -55,24 +56,27 @@ fn main() {
         messages.push(Integer::from_str_radix(&hex::encode(input), 16).unwrap());
     }
     let mut rand = rand::RandState::new();
-    for i in 0..number {
+    alice_sends_bob.push(base64::encode(n.to_string()));
+    alice_sends_bob.push(base64::encode(e.to_string()));
+    for _ in 0..number {
         let xi = n.clone().random_below(&mut rand);
-        print!("\nx{i}: {}", base64::encode(xi.to_string()));
+        alice_sends_bob.push(base64::encode(xi.to_string()));
         randoms.push(xi);
     }
-    print!("\n\nEnter v: ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
-    let input = String::from_utf8(base64::decode(input.trim()).unwrap()).unwrap();
-    let input = input.as_str();
-    let v = Integer::from_str_radix(input, 10).unwrap();
+    let joined_string = alice_sends_bob.join(" ");
+    println!("\nSending public key and random messages to Bob...");
+    stream.write_all(joined_string.as_bytes()).expect("failed to write");
+    stream.flush().unwrap();
+    let stream = TcpStream::connect("127.0.0.1:6969").expect("Failed to connect");
+    let mut reader = BufReader::new(&stream);
+    let mut buffer: Vec<u8> = Vec::new();
+    reader.read_until(b'\n',&mut buffer).unwrap();
+    let string = String::from_utf8(base64::decode(buffer).unwrap()).unwrap();
+    let v = Integer::from_str_radix(string.as_str(), 10).unwrap();
     let ciphertexts = combine_values(&v, &randoms, &d, &n, &messages);
-    println!();
-    for (index, element) in ciphertexts.iter().enumerate() {
-        println!("ciphertext {index}: {}", base64::encode(element.clone().to_string()));
-        io::stdout().flush().unwrap();
-    }
+    let mut stream = TcpStream::connect("127.0.0.1:6969").expect("Failed to connect");
+    let joined_string = ciphertexts.join(" ");
+    println!("\nSending ciphertexts to Bob...");
+    stream.write_all(joined_string.as_bytes()).expect("failed to write");
+    println!("\nConnection terminated.");
 }
--- bob/src/main.rs
+++ bob/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
-use std::io::Write;
+use std::net::TcpListener;
+use std::io::{Read,Write};
 use rug::{Integer, rand};
 
 fn encrypt_and_blind(randoms: &Vec<Integer>, pk: &(Integer, Integer), b: u32, k: &Integer) -> Integer {
@@ -26,40 +27,25 @@ fn base64_to_integer(input: &str) -> Integer {
 }
 
 fn main() {
-    println!("\nEnter the public key (n, g): ");
-    print!("Enter n: ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
-    let n = base64_to_integer(input.as_str());
-    print!("Enter e: ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
-    let e = base64_to_integer(input.as_str());
+    let receiver_listener = TcpListener::bind("127.0.0.1:6969").expect("Failed and bind with the sender");
+    println!("Listening on localhost:6969...");
+    let mut stream = receiver_listener.accept().unwrap().0;
+    let mut buf = [0; 4096];
+    let mut received_bytes = Vec::new();
+    let bytes_read = stream.read(&mut buf).unwrap();
+    received_bytes.extend_from_slice(&buf[..bytes_read]);
+    let zero_pos = received_bytes.iter().position(|&b| b == 0).unwrap_or(received_bytes.len());
+    let received_string = String::from_utf8_lossy(&received_bytes[..zero_pos]).to_string();
+    let received_vec: Vec<&str> = received_string.split(' ').collect();
+    let n = base64_to_integer(received_vec[0]);
+    let e = base64_to_integer(received_vec[1]);
     let pk = (n.clone(), e);
     let mut randoms: Vec<Integer> = Vec::new();
-    print!("\nHow many messages? ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
-    let number = input.trim().parse().unwrap();
+    let number = received_vec.len() - 2;
     for i in 0..number {
-        print!("\nEnter x{i}: ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .unwrap();
-        randoms.push(base64_to_integer(input.as_str()));
+        randoms.push(base64_to_integer(received_vec[i + 2]));
     }
-    print!("\nEnter b: ");
+    print!("\nEnter the index of the message to be retrieved: ");
     io::stdout().flush().unwrap();
     let mut input = String::new();
     io::stdin()
@@ -69,18 +55,24 @@ fn main() {
     let mut rand = rand::RandState::new();
     let k = n.random_below(&mut rand);
     let v = base64::encode(encrypt_and_blind(&randoms, &pk, b, &k).to_string());
-    println!("\nv: {v}");
+    let mut stream = receiver_listener.accept().unwrap().0;
+    println!("\nSending v to Alice...");
+    stream.write_all(v.as_bytes()).expect("failed to write");
+    stream.flush().unwrap();
+    drop(stream);
+    let mut stream = receiver_listener.accept().unwrap().0;
+    let mut buf = [0; 4096];
+    let bytes_read = stream.read(&mut buf).unwrap();
+    received_bytes = Vec::new();
+    received_bytes.extend_from_slice(&buf[..bytes_read]);
+    let zero_pos = received_bytes.iter().position(|&b| b == 0).unwrap_or(received_bytes.len());
+    let received_string = String::from_utf8_lossy(&received_bytes[..zero_pos]).to_string();
+    let received_vec: Vec<&str> = received_string.split(' ').collect();
     let mut ciphertexts: Vec<Integer> = Vec::new();
-    for i in 0..number {
-        print!("\nEnter ciphertext {i}: ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .unwrap();
-        ciphertexts.push(base64_to_integer(input.as_str()));
+    for element in received_vec {
+        ciphertexts.push(base64_to_integer(element));
     }
     let output_plaintext = decrypt_message(&ciphertexts, &k, b);
     let output_plaintext = format!("{:X}", &output_plaintext);
-    println!("\nChosen message: {}", String::from_utf8(hex::decode(output_plaintext).unwrap()).unwrap());
+    println!("\nRetrieved message: {}", String::from_utf8(hex::decode(output_plaintext).unwrap()).unwrap());
 }
